bdaadccb65cce6311731fe2f09123b370236bfc9
TahaMedhous <medhoust@gmail.com>
2022-05-29 21:38:30 +0100

    STUF

---
+++ Maqalat/أدباء-محمود-شاكر/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+<meta viewport="width=device-width, initial-scale=1.0">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../style/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="titleH">
+            <span style="text-shadow: 1px 1px 0px #a4748c, 2px 2px 0px #a4748c, 3px 3px 0px #a4748c, 4px 4px 0px #a4748c;">السرمديات</span>
+        </div>
+    </header>
+    <hr style="    margin-bottom: 0%;
+;">
+    <section>
+
+        <div class="imgP">
+				<span style="font-size: 5vw;font-weight: 600;">
+    الطريق إلى الحق
+</span>
+<hr>
+            <img src="../../img/01.jpg" alt="الشيخ محمود شاكر مع الدكتور محمد مكي">
+            <span class="imgC">الشيخ محمود شاكر مع الدكتور محمد مكي</span>
+        </div>
+		
+        <p>
+            قرأت في مجلة الثقافة العدد "209" كلمة تحت عنوان "الصحافة والأدب في أسبوع"، فرأيت كتابًا من صديقي الشاعر الأستاذ محمود حسن إسماعيل إلى صديقي أيضًا. . . الأستاذ (ق). وفي هذا الكتاب ذكْرُ بعض أصحابنا وذكرى، ويصفنا الصديق الأستاذ الشاعر بصفات جميلة محببة كاللجاج، والتهاتر، والكسل، والجبن، والغفلة، والتخلف عن سير الزمان، ويدعونا إلى ملازمة الصمت على رفوفنا الجامدة حتى يتحرك بنا أو ينسانا الزمان! . . . وهو كذلك لا أدري! فقد سمعت أن الأوائل قالوا: "عقل المرء مخبوءٌ تحت لسانه"، وأنهم قالوا:
+        </p>
+        <div class="quote-container">
+        <div class="quote">
+            <table class="poem">
+                <tbody>
+                    <tr><td>إِذا لَم يَكُن لِلمَرءِ عَقلٌ يَكفُّه</td></tr>
+                    <tr><td>عن الجهل، لم يَسْتَحْيي وانهتَكَ الستر</td></tr>
+                </tbody>
+            </table>
+        </div>
+        </div>
+        <p>
+            وللصديقين منِّي تحية المخلص المعجب بأدبهما وبيانهما.
+        </p>
+        <p>
+            أما مسألة الخطأ والصواب في اللغة، ومسألة عنصر الثبات فيها، فنتركها إلى العدد التالى من الرسالة، ولأخي مندور تحيتي وشكري.
+
+        </p>
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
